e156ede8f1981b59fdb06d4c7ce9d1ee68d361fa
Cawan0 <dudajakub@o2.pl>
2023-10-20 07:12:56 +0200

    final version
    Description added nested array option

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ const TestCase4 = [2, 5, 4, 9, 7, 7, 8, 4, 5, 0, 5, 7, 6, 3, 2, 7, 0, 9, 5, 5, 7
 const TestCase5 = [2, 10, 10, 6, 6, 1, 7, 1, 8, 5, 0, 8, 0, 7, 2, 6, 4, 2, 4, 7, 0, 3, 0, 9, 8, 1, 0, 3, 7, 6, 6, 4, 7, 3, 7, 5, 7, 1, 0, 3, 4, 3, 1, 5, 3, 3, 7, 9, 3, 2, 5, 9, 6, 7, 6, 3, 1, 0, 6, 0, 2, 4, 1, 8, 3, 1, 9, 7, 2, 2, 9, 0, 3, 9, 9, 0, 9, 0, 3, 9, 10, 6, 4, 3, 0, 5, 10, 1, 8, 9, 5, 5, 5, 7, 10, 5, 8, 4, 5, 10]
 const TestCase6 = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
 const TestCase7 = [2, 7, 8, 2, 6, 2, 6, 1, 2, 9, 4, 3, 4, 0, 8, 3, 7, 6, 7, 6, 0, 5, 5, 4, 7, 4]
+const TestCase8 = [[2, 7, 8, 2, 6, 2, 6],[6, 1, 2, 9, 4, 3, 4, 0, 8, 3, 7, 6],[6, 7, 6, 0, 5, 5, 4, 7, 4]]
+const TestCase9 = [[6, 4, 2, 0, 3, 2, 0, 3, 1, 4, 5, 3, 2, 7, 5, 3, 0, 1, 2, 1, 3, 4, 6, 8, 1, 3],[6, 2, 1, 1, 8, 0, 5, 5, 0, 1, 8, 9, 6, 9, 4, 8, 0, 0]]
+
 
 function check(arr){
     let material=0 
-    let x =arr[0]
+    let x=arr[0]
     let y=0;
-    let z = [];
+    let z=[];
+
+    if(Array.isArray(arr[0])){
+        let single_array=[]
+        arr.forEach(e=>{
+            single_array=single_array.concat(e)
+        })
+        material = check(single_array)
+    }
     for(let i=1;i<arr.length;i++){
         if(x<=arr[i]){
             z.push([y,i])
@@ -62,3 +73,5 @@ console.log("Test 5: "+check(TestCase4))
 console.log("Test 6: "+check(TestCase5))
 console.log("Test 7: "+check(TestCase6))
 console.log("Test 8: "+check(TestCase7))
+console.log("Test 9: "+check(TestCase8))
+console.log("Test 10: "+check(TestCase9))
